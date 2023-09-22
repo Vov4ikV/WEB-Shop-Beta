277efe8298b59e2743b7b2bf3175656cc5608aa0
Alex <lenkoran26@outlook.com>
2023-09-22 21:48:52 +0300

    22/09/2023

--- cart/models.py
+++ cart/models.py
@@ -4,11 +4,10 @@ from products.models import Products
 
 
 class CartUser(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(CartUser, on_delete=models.CASCADE, related_name='cart')
-    product = models.OneToOneField(Products, on_delete=models.CASCADE, related_name='products')
-    quantity = models.ImageField()
-
+    cart = models.ForeignKey(CartUser, on_delete=models.CASCADE)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
